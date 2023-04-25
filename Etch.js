9c6c633b425b78fa0ac01fe2cd463fb8479e9141
John Anderson <djanyreason@gmail.com>
2023-04-25 12:29:27 -0400

    Enforces max etch grid dimension of 100 boxes
    Later instructions in the Odin Project project specified a max
    Etch-a-Sketch grid size of 100x100 boxes. This commit adds this
    limit to buildEAS.

--- Etch.js
+++ Etch.js
@@ -1,33 +1,37 @@
 //Builds an Etch-a-Sketch in the HTML of grid size dimension x dimension
-//Takes one parameter: dimension, which should be an integer
+//Takes one parameter: dimension, which should be an integer, and is max 100
 function buildEAS(dimension) {
- const frame = document.getElementById("etchFrame");
+  if(dimension > 100) return buildEAS(100);
 
- //Define the size of each square in the Etch a Sketch so the total size is as
- //close to 512px as possible without going over; minimum size of 2px is 
- //enforced for aesthetic purposes (this can cause box to exceed 512px); 1px is
- //subtracted to account for the 1px border
- const boxSize = ((dimension * Math.round(512 / dimension)) <= 512) ? 
-                 Math.max((Math.round(512 / dimension) - 1),2) : 
-                 Math.max((Math.round(512 / dimension) - 2),2);
+  const frame = document.getElementById("etchFrame");
 
- for(let i = 0; i < dimension; i++) { // for loop to add rows to frame
-  const row = document.createElement("div");
-  (i === 0) ? row.className = "etchRow topRow" : row.className = "etchRow";
-  for(let j = 0; j < dimension; j++) { //for loop to add boxes to each row
-    const box = document.createElement("div");
-    box.className = "etchBox";
-    box.style.height = boxSize + "px";
-    box.style.width = boxSize + "px";
+  //Define the size of each square in the Etch a Sketch so the total size is as
+  //close to 512px as possible without going over; minimum size of 2px is 
+  //enforced for aesthetic purposes (this can cause box to exceed 512px); 1px is
+  //subtracted to account for the 1px border
+  const boxSize = ((dimension * Math.round(512 / dimension)) <= 512) ? 
+                  Math.max((Math.round(512 / dimension) - 1),2) : 
+                  Math.max((Math.round(512 / dimension) - 2),2);
 
-    // Add drawing/Etch-a-Sketching functionality
-    box.addEventListener("mouseover", () => 
-                         { box.style.backgroundColor = "black"; });
+  for(let i = 0; i < dimension; i++) { // for loop to add rows to frame
+    const row = document.createElement("div");
+    (i === 0) ? row.className = "etchRow topRow" : row.className = "etchRow";
     
-    row.appendChild(box);
+    for(let j = 0; j < dimension; j++) { //for loop to add boxes to each row
+      const box = document.createElement("div");
+      box.className = "etchBox";
+      box.style.height = boxSize + "px";
+      box.style.width = boxSize + "px";
+
+      // Add drawing/Etch-a-Sketching functionality
+      box.addEventListener("mouseover", () => 
+                           { box.style.backgroundColor = "black"; });
+    
+      row.appendChild(box);
+    }
+  
+    frame.appendChild(row);
   }
-  frame.appendChild(row);
- }
 }
 
 buildEAS(16); //start with a 16x16 grid
