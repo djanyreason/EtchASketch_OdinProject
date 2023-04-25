2e87463ec678cc4650c5031e2fb8f4fa673f0e25
John Anderson <djanyreason@gmail.com>
2023-04-25 12:51:12 -0400

    Makes etch grid dimensions always 513px x 513px
    Later instructions in the Odin Project project specified that the
    size of the grid should not change regardless of the number of boxes
    in the Etch-a-Sketch grid. This commit makes it so the total grid is
    always 513px x 513px, adding border around the grid to fill necessary
    space.
    
    This commit also does a little cleanup in buildEAS, using Math.floor
    to calculate the etch box div size, rather than a more complicated
    comparison operation using Math.round.

--- Etch.js
+++ Etch.js
@@ -1,21 +1,32 @@
-//Builds an Etch-a-Sketch in the HTML of grid size dimension x dimension
+//Builds an Etch-a-Sketch in the HTML of grid size dimension x dimension, with
+//the total Etch-a-Sketch size being 513px x 513px
 //Takes one parameter: dimension, which should be an integer, and is max 100
 function buildEAS(dimension) {
   if(dimension > 100) return buildEAS(100);
 
   const frame = document.getElementById("etchFrame");
 
-  //Define the size of each square in the Etch a Sketch so the total size is as
-  //close to 512px as possible without going over; minimum size of 2px is 
-  //enforced for aesthetic purposes (this can cause box to exceed 512px); 1px is
-  //subtracted to account for the 1px border
-  const boxSize = ((dimension * Math.round(512 / dimension)) <= 512) ? 
-                  Math.max((Math.round(512 / dimension) - 1),2) : 
-                  Math.max((Math.round(512 / dimension) - 2),2);
+  //Define the size of each square in the Etch a Sketch so the total size of
+  //all boxes is as close to 513px as possible without going over; uses 512
+  //instead of 513 to account for 1px borders around each etch box; 1px is 
+  //subtracted from calculation to account for the 1px etch box border
+  const boxSize = Math.floor(512 / dimension) - 1;
+
+  //Get the total borders needed around the grid to maintain 512px
+  const gridBorder = 513 - (dimension * (boxSize + 1));
 
   for(let i = 0; i < dimension; i++) { // for loop to add rows to frame
     const row = document.createElement("div");
-    (i === 0) ? row.className = "etchRow topRow" : row.className = "etchRow";
+    row.className = "etchRow";
+
+    //set borders around the grid to maintain 512px
+    row.style.borderLeft = Math.floor(gridBorder/2) + "px solid black";
+    row.style.borderRight = Math.round(gridBorder/2) + "px solid black";
+    if(i === 0) row.style.borderTop = Math.round(gridBorder / 2) + 
+                                      "px solid black";
+    else if (i === dimension - 1) 
+                row.style.borderBottom = Math.floor(gridBorder / 2) + 
+                                      "px solid black";
     
     for(let j = 0; j < dimension; j++) { //for loop to add boxes to each row
       const box = document.createElement("div");
