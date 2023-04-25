529532a2e2fcbed7cdf62ab66b3c405bd5e8b3a9
John Anderson <djanyreason@gmail.com>
2023-04-25 09:58:24 -0400

    Asjusts EtchBox sizes to fit 512px x 512px frame
    Previously, EtchBoxes were of fixed size. This commit modifies the
    buildEAS function so that it sets the height and width style of each
    etchBox div so that the total height and width of all boxes is as
    close to 512px as possible without going over.

--- Etch.js
+++ Etch.js
@@ -3,15 +3,24 @@
 function buildEAS(dimension) {
  const frame = document.getElementById("etchFrame");
 
+ //Define the size of each square in the Etch a Sketch so the total size is as
+ //close to 512px as possible without going over; remove 1px extra for border
+ const boxSize = ((dimension * Math.round(512 / dimension)) <= 512) ? 
+                 (Math.round(512 / dimension) - 1) : 
+                 (Math.round(512 / dimension) - 2);
+
  for(let i = 0; i < dimension; i++) { // for loop to add rows to frame
   const row = document.createElement("div");
-  row.className = "etchRow";
+  (i === 0) ? row.className = "etchRow topRow" : row.className = "etchRow";
   for(let j = 0; j < dimension; j++) { //for loop to add boxes to each row
     const box = document.createElement("div");
     box.className = "etchBox";
+    box.style.height = boxSize + "px";
+    box.style.width = boxSize + "px";
 
     // Add drawing/Etch-a-Sketching functionality
-    box.addEventListener("mouseover", () => { box.style.backgroundColor = "black"; });
+    box.addEventListener("mouseover", () => 
+                         { box.style.backgroundColor = "black"; });
     
     row.appendChild(box);
   }
